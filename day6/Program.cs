5697260ae30ba60b523e2da73c5bbd0bc28567ee
Martin Meixger <mail@meixger.com>
2022-12-06 19:58:03 +0100

    day6 net

---
+++ day6/Program.cs
@@ -0,0 +1,13 @@
+﻿var path = Path.Combine(Environment.CurrentDirectory, @".\input.txt");
+var input = File.ReadAllText(path);
+
+int FindUniqueSequence(string input, int uniqueLength)
+{
+    for (var i = 0; i < input.Length; i++)
+        if (input[i..(i + uniqueLength)].Distinct().Count() == uniqueLength)
+            return i + uniqueLength;
+    return -1;
+}
+
+Console.WriteLine($"Solution1: {FindUniqueSequence(input, 4)}");
+Console.WriteLine($"Solution2: {FindUniqueSequence(input, 14)}");
